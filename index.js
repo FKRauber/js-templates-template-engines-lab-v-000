f9c197df69f011ab62b428b1ee1abb36685d7237
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-28 16:35:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function createPost() {
-  var postTitle = document.getElementById('postTitle').value;
-  var postAuthor = document.getElementById('postAuthor').value;
-  var post = document.getElementById('postBody').value;
-
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
   var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  var postTitle = document.getElementById('postTitle').value;
+  var postAuthor = document.getElementById('postAuthor').value;
+  var post = document.getElementById('postBody').value;
 
   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
