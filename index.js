db305ecf19ad617d012dc5682c05a14d82a87cd8
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-28 16:29:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function postComment() {
   var commenter = document.getElementById('commentAuthor').value;
   var allComments = document.getElementById('comments');
 
-  let commentTemplate = document.getElementById('comment-template').innerHTML;
-  let templateFn = _.template(commentTemplate);
+  var commentTemplate = document.getElementById('comment-template').innerHTML;
+  var templateFn = _.template(commentTemplate);
 }
